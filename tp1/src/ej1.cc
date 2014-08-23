7f4fa5f97508b1ba9c4dae254dbe4ddf8946681e
Pablo Gonzalez Alba <pablo.gonzalez.alba@gmail.com>
2014-08-23 10:39:39 -0300

    Modifico el algoritmo del Ej1 para calcular primero si tiene solución Según lo hablado en clase, primero se calcula en O(n) si es posible cruzar Además agregué la función puede_saltar() para que quede más evidente en los condicionales Puse eso también en el informe Y agregué un par de tests

--- tp1/src/ej1.cc
+++ tp1/src/ej1.cc
@@ -6,30 +6,32 @@
 using namespace std;
 
 
-vector<int> cruzar_puente   (int salto_maximo, vector<int>& puente);
-void        imprimir_saltos (vector<int>& saltos);
+vector<unsigned int> cruzar_puente   (unsigned int salto_maximo, vector<unsigned int>& puente);
+bool                 posible_cruzar  (unsigned int salto_maximo, vector<unsigned int>& puente);
+bool                 puede_saltar    (vector<unsigned int>& puente, unsigned int tablon);
+void                 imprimir_saltos (vector<unsigned int>& saltos);
 
 
 int main () {
-  int n, c;
-  
+  unsigned int n, c;
+
   cin >> n;
-  
+
   while (n != 0) {
     cin >> c;
-    vector<int> puente;
+    vector<unsigned int> puente;
     puente.resize(n);
-    
-    for (int i = 0; i < n; i++) {
+
+    for (unsigned int i = 0; i < n; i++) {
       cin >> puente[i];
     }
 
-    vector<int> saltos = cruzar_puente(c, puente);
+    vector<unsigned int> saltos = cruzar_puente(c, puente);
     imprimir_saltos(saltos);
-    
+
     cin >> n;
   }
-    
+
   return 0;
 }
 
@@ -38,36 +40,65 @@ int main () {
  * Devuelve una lista con los tablones a los cuales se debe saltar
  * ó una lista vacía si es imposible.
  **/
-vector<int> cruzar_puente (int salto_maximo, vector<int>& puente) {
-  if (salto_maximo <= 0)
-	return vector<int>();
-  
-  vector<int> saltos;
-  
+vector<unsigned int> cruzar_puente (unsigned int salto_maximo, vector<unsigned int>& puente) {
+  if (!posible_cruzar(salto_maximo, puente)) {
+    return vector<unsigned int>();
+  }
+
+  vector<unsigned int> saltos;
+
   unsigned int salto_actual;
   unsigned int posicion_actual = 0;
-  
+
   while (posicion_actual <= puente.size()) {
     salto_actual = salto_maximo;
-    
-    /* Se chequea si el tablón al cual voy a saltar está ó no sano. */
-    while (puente[posicion_actual + salto_actual - 1] == 1) {
+
+    /* Se prueba desde el último para atrás hasta encontrar un lugar para saltar */
+    while (!puede_saltar(puente, posicion_actual + salto_actual - 1)) {
       salto_actual--;
-      if (salto_actual == 0) {
-        return vector<int>();
-      }
     }
 
     posicion_actual += salto_actual;
     /* Se agrega al vector de resultados el tablón al cual se puede saltar. */
     saltos.push_back(posicion_actual);
   }
-  
+
   return saltos;
 }
 
 
-void imprimir_saltos (vector<int>& saltos) {
+/**
+ * Calcula si los tablones rotos consecutivos son siempre menores al salto
+ * máximo, es decir, si se puede cruzar el puente
+ **/
+bool posible_cruzar (unsigned int salto_maximo, vector<unsigned int>& puente) {
+  unsigned int tablones_rotos_consecutivos = 0;
+
+  for (unsigned int i = 0; i < puente.size(); i++) {
+    if (puede_saltar(puente, i)) {
+      tablones_rotos_consecutivos = 0;
+    } else {
+      tablones_rotos_consecutivos++;
+    }
+
+    if (tablones_rotos_consecutivos >= salto_maximo) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+/**
+ * Devuelve verdadero cuando es un tablón sano o si se terminó el puente
+ **/
+bool puede_saltar (vector<unsigned int>& puente, unsigned int tablon) {
+  return tablon >= puente.size() || puente[tablon] == 0;
+}
+
+
+void imprimir_saltos (vector<unsigned int>& saltos) {
   if (saltos.empty()) {
     cout << "no" << endl;
   } else {

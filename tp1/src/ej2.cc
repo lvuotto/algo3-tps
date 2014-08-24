c30060d8fdde20080aa5fc5748db049ff814e5bc
Agustín Vaghi <vaghito@gmail.com>
2014-08-24 20:04:10 -0300

    Modificaciones varias.
     - Se comenzó con el problema 3.
     - Se pulieron las ideas de los problemas 1 y 2.

--- tp1/src/ej2.cc
+++ tp1/src/ej2.cc
@@ -11,28 +11,27 @@ enum posicion_t {IZQUIERDA, DERECHA};
 
 enum direccion_t {HORIZONTAL, VERTICAL};
 
-struct Coordenada {
+struct Punto {
   unsigned int x;
   unsigned int y;
   posicion_t posicion;
 
-  Coordenada(unsigned int _x, unsigned int _y, posicion_t _posicion) {
+  Punto(unsigned int _x, unsigned int _y, posicion_t _posicion) {
     x = _x;
     y = _y;
     posicion = _posicion;
   }
 
-  Coordenada() {
+  Punto() {
   }
 
-  bool operator < (const Coordenada& otra) const {
-    return x < otra.x || (x == otra.x && y < otra.y);
+  bool operator < (const Punto& otro) const {
+    return x < otro.x || (x == otro.x && y < otro.y);
   }
 };
 
-vector<unsigned int> calcular_horizonte (vector<Coordenada>& coordenadas);
-void                 agregar_avance     (vector<unsigned int>& avances, unsigned int x, unsigned int y, direccion_t& direccion);
-void                 imprimir_avances   (vector<unsigned int>& avances);
+vector<unsigned int> calcular_horizonte (vector<Punto>& puntos);
+void                 imprimir_horizonte (vector<unsigned int>& solucion);
 unsigned int         maximo             (multiset<unsigned int>& alturas);
 
 
@@ -42,20 +41,20 @@ int main () {
   cin >> cant_edificios;
 
   while (cant_edificios != 0) {
-    vector<Coordenada> coordenadas;
-    coordenadas.resize(cant_edificios * 2);
+    vector<Punto> puntos;
+    puntos.resize(cant_edificios * 2);
 
     for (unsigned int i = 0; i < cant_edificios; i++) {
       /* Para cada edificio se agregan las coordenadas de los dos puntos
          que lo definen */
       cin >> izquierda >> altura >> derecha;
 
-      coordenadas[i * 2] = Coordenada(izquierda, altura, IZQUIERDA);
-      coordenadas[i * 2 + 1] = Coordenada(derecha, altura, DERECHA);
+      puntos[i * 2] = Punto(izquierda, altura, IZQUIERDA);
+      puntos[i * 2 + 1] = Punto(derecha, altura, DERECHA);
     }
 
-    vector<unsigned int> avances = calcular_horizonte(coordenadas);
-    imprimir_avances(avances);
+    vector<unsigned int> solucion = calcular_horizonte(puntos);
+    imprimir_horizonte(solucion);
 
     cin >> cant_edificios;
   }
@@ -64,107 +63,63 @@ int main () {
 }
 
 /**
- * Devuelve los avances en X y en Y que deben realizarse para formar el
- * horizonte de los edificios.
+ * Devuelve los puntos necesarios para formar el horizonte de los edificios.
  **/
-vector<unsigned int> calcular_horizonte (vector<Coordenada>& coordenadas) {
+vector<unsigned int> calcular_horizonte (vector<Punto>& puntos) {
   multiset<unsigned int> alturas;
 
-  Coordenada* ultima_coordenada = NULL;
+  vector<unsigned int> solucion;
+  
+  sort(puntos.begin(), puntos.end());
+  
+  auto punto = puntos.begin();
+  
+  solucion.push_back(punto->x);
+  solucion.push_back(punto->y);
+  
+  while (punto->x == solucion[0]) {
+    solucion[1] = punto->y;
+    alturas.insert(punto->y);
+    punto++;
+  }
 
-  vector<unsigned int> avances;
-
-  direccion_t direccion_avanzar = HORIZONTAL;
-
-  sort(coordenadas.begin(), coordenadas.end());
-
-  for (auto coordenada = coordenadas.begin(); coordenada != coordenadas.end(); coordenada++) {
-    if (ultima_coordenada == NULL) {
-      ultima_coordenada = &(*coordenada);
-
-      alturas.insert(coordenada->y);
-    } else if (coordenada->posicion == IZQUIERDA) {
+  while (punto != puntos.end()) {
+    if (punto->posicion == IZQUIERDA) {
       /**
-       * Situaciones cuando la nueva coordenada es IZQUIERDA:
-       * ----------------------------------------------------------------------
-       * Si la nueva es más alta y más a la derecha:
-       *                  NUEVA_COORDENADA
-       *                        |
-       *                        |
-       * ULTIMA_COORDENADA------A
-       *
-       * Se agrega el punto A al horizonte.
-       * Y el puntero a la última ahora apunta a la nueva.
-       *
-       * ----------------------------------------------------------------------
-       * Si la nueva es igual o menos alta:
-       * ULTIMA_COORDENADA------NUEVA_COORDENADA
-       * ó
-       * ULTIMA_COORDENADA------A
-       *                        |
-       *                        |
-       *                  NUEVA_COORDENADA
-       *
-       * Únicamente se agrega su altura al multiconjunto de alturas.
+       * 
        **/
-      if (coordenada->y > ultima_coordenada->y) {
-        if (coordenada->x > ultima_coordenada-> x) {
-          agregar_avance(avances, ultima_coordenada->x, ultima_coordenada->y, direccion_avanzar);
-
-          agregar_avance(avances, coordenada->x, ultima_coordenada->y, direccion_avanzar);
+      if (punto->y > solucion.back()) {
+        if (punto->x > solucion[solucion.size() - 2]) {
+          solucion.push_back(punto->x);
+          solucion.push_back(punto->y);
+        } else {
+          solucion[solucion.size() - 1] = punto->y;
         }
-
-        ultima_coordenada = &(*coordenada);
       }
 
-      alturas.insert(coordenada->y);
+      alturas.insert(punto->y);
     } else {
       /**
-       * Situaciones cuando la nueva coordenada es DERECHA:
-       * ----------------------------------------------------------------------
-       * Si la nueva es la única con la altura actual:
-       * ALTURA_ACTUAL-----NUEVA_COORDENADA
-       *                           |
-       *                           |
-       *                           A--------NUEVA_ALTURA_MÁXIMA
-       *
-       * Se agrega el punto de la nueva coordenada al horizonte.
-       * Y el puntero a la última ahora apunta al punto A.
-       *
-       * ----------------------------------------------------------------------
-       * Si la nueva es menos alta:
-       * ALTURA_ACTUAL-----NUEVA_ALTURA_MÁXIMA
-       *                           |
-       *                           |
-       *                    NUEVA_COORDENADA
-       * ó si la siguiente altura máxima se mantiene
-       *
-       * Únicamente se elimina su altura del multiconjunto de alturas.
+       * 
        **/
-      if (coordenada->y == maximo(alturas)) {
-        alturas.erase(alturas.find(coordenada->y));
+      if (punto->y == maximo(alturas)) {
+        alturas.erase(alturas.find(punto->y));
 
-        if (coordenada->y > maximo(alturas)) {
-          agregar_avance(avances, ultima_coordenada->x, ultima_coordenada->y, direccion_avanzar);
-
-          agregar_avance(avances, coordenada->x, coordenada->y, direccion_avanzar);
-
-          ultima_coordenada = new Coordenada(coordenada->x, maximo(alturas), IZQUIERDA);
+        if (punto->y > maximo(alturas)) {
+          solucion.push_back(punto->x);
+          solucion.push_back(maximo(alturas));
         }
       } else {
         if (!alturas.empty()) {
-          alturas.erase(alturas.find(coordenada->y));
+          alturas.erase(alturas.find(punto->y));
         }
       }
     }
+    
+    punto++;
   }
 
-  /* Se agregan los avances necesarios para ir hasta la última coordenada y de
-   * allí hacia el 0 final */
-  agregar_avance(avances, ultima_coordenada->x, ultima_coordenada->y, direccion_avanzar);
-  agregar_avance(avances, ultima_coordenada->x, 0, direccion_avanzar);
-
-  return avances;
+  return solucion;
 }
 
 
@@ -181,28 +136,14 @@ unsigned int maximo (multiset<unsigned int>& alturas) {
 
 
 /**
- * Agrega el avance necesario correspondiente a la coordenada X e Y.
+ * Imprime los puntos requeridos para marcar el horizonte.
  **/
-void agregar_avance (vector<unsigned int>& avances, unsigned int x, unsigned int y, direccion_t& direccion) {
-  if (direccion == HORIZONTAL) {
-    avances.push_back(x);
-    direccion = VERTICAL;
-  } else {
-    avances.push_back(y);
-    direccion = HORIZONTAL;
-  }
-}
-
-
-/**
- * Imprime los avances requeridos para realizar el horizonte.
- **/
-void imprimir_avances (vector<unsigned int>& avances) {
-  auto it = avances.begin();
+void imprimir_horizonte (vector<unsigned int>& solucion) {
+  auto it = solucion.begin();
 
   cout << *it;
 
-  for (it++; it != avances.end(); it++) {
+  for (it++; it != solucion.end(); it++) {
     cout << " " << *it;
   }
 

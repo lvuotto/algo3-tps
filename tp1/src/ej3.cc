694cc3487e27857153bddf5531aa10e4ab2529b9
Lucas Gabriel Vuotto <lvuotto@dc.uba.ar>
2014-08-31 01:02:53 -0300

    Corrección de `tp1/src/ej3.cc`.
    Aparentemente "funciona". Se hicieron los siguientes cambios:
     - se agregló la comprobación de `elementos` vacíos en camion::entra.
     - se arregló el cálculo del índice de la matriz de peligrosidades en
       camion::calcularPeligrosidad.

--- tp1/src/ej3.cc
+++ tp1/src/ej3.cc
@@ -6,52 +6,54 @@
 using namespace std;
 
 
-typedef vector< vector<int> > matriz;
+typedef vector<vector<int> > matriz;
 
 
 int kMaximaPeligrosidad;
-matriz matrizPeligrosidades;
+matriz peligrosidades;
 
 
 struct camion {
-  camion() : elementos(), peligrosidad(0) {};
+  camion () : peligrosidad_(0) {};
   
-  void agregarElemento(int& elemento) {
+  void agregarElemento (int elemento) {
+    peligrosidad_ += calcularPeligrosidad(elemento);
     elementos.insert(elemento);
-    peligrosidad += calcularPeligrosidad(elemento);
   }
   
-  void eliminarElemento(int& elemento) {
+  void eliminarElemento (int elemento) {
     elementos.erase(elemento);
-    
-    peligrosidad -= calcularPeligrosidad(elemento);
+    peligrosidad_ -= calcularPeligrosidad(elemento);
   }
   
-  bool entra(int& elemento) const {
-    return peligrosidad + calcularPeligrosidad(elemento) <= kMaximaPeligrosidad;
+  bool entra (int elemento) const {
+    return elementos.empty() ||
+           (peligrosidad_ + calcularPeligrosidad(elemento) <=
+            kMaximaPeligrosidad);
   }
   
-  int calcularPeligrosidad(int& elemento) const {
+  int calcularPeligrosidad (int elemento) const {
     int suma = 0;
     
     for (auto el = elementos.begin(); el != elementos.end(); el++) {
-      cout << *el << endl;
-      suma += matrizPeligrosidades[elemento][*el];
+      suma += peligrosidades[*el][elemento - 1];
     }
   
     return suma; 
   }
   
-  set <int> elementos;
+  set<int> elementos;
   
 private:
-  int peligrosidad;
+  int peligrosidad_;
 };
 
 
-vector<camion> biohazard (set<int>& elementos);
-bool backtracking (vector<camion>& camiones, set<int>& elementos);
-void mostrar_solucion (vector<camion>& camiones, int cantidadElementos);
+vector<camion> biohazard        (set<int>& elementos);
+bool           backtracking     (vector<camion>& camiones,
+                                 set<int>& elementos);
+void           mostrar_solucion (vector<camion>& camiones,
+                                 int cantidadElementos);
 
 
 int main ()
@@ -61,21 +63,21 @@ int main ()
   cin >> cantidadElementos;
   while (cantidadElementos != 0) {
     cin >> kMaximaPeligrosidad;
-    matrizPeligrosidades.resize(cantidadElementos - 1);
+    peligrosidades.resize(cantidadElementos - 1);
     
-    for (auto it = matrizPeligrosidades.begin(); it != matrizPeligrosidades.end(); it++) {
+    for (auto it = peligrosidades.begin(); it != peligrosidades.end(); it++) {
       it->resize(cantidadElementos - 1);
     }
     
     for (int i = 0; i < cantidadElementos - 1; i++) {
       for (int j = 0; j < i; j++) {
-        matrizPeligrosidades[i][j] = 0;
+        peligrosidades[i][j] = 0;
       }
       for (int j = i; j < cantidadElementos - 1; j++) {
-        cin >> matrizPeligrosidades[i][j];
+        cin >> peligrosidades[i][j];
       }
     }
-    
+
     set<int> elementos;
     for (int i = 0; i < cantidadElementos; i++) {
       elementos.insert(i);
@@ -91,9 +93,9 @@ int main ()
 }
 
 
-vector <camion> biohazard (set<int>& elementos)
+vector<camion> biohazard (set<int>& elementos)
 {
-  vector <camion> camiones;
+  vector<camion> camiones;
   do {    
     camiones.push_back(camion());
   } while (!backtracking(camiones, elementos));
@@ -106,9 +108,10 @@ bool backtracking (vector <camion>& camiones, set <int>& elementos)
   if (elementos.empty()) {
     return true;
   }
+  
   int elemento = *elementos.begin();
   elementos.erase(elementos.begin());
-  for (vector<camion>::iterator c = camiones.begin(); c != camiones.end(); c++) {
+  for (auto c = camiones.begin(); c != camiones.end(); c++) {
     if (c->entra(elemento)) {
       c->agregarElemento(elemento);
       if (backtracking(camiones, elementos)) {
@@ -119,6 +122,7 @@ bool backtracking (vector <camion>& camiones, set <int>& elementos)
     }
   }
   elementos.insert(elemento);
+  
   return false;
 }
 

52de8c583c3f8434497ec2a7d36b124b74eba3fc
lvuotto <lvuotto@dc.uba.ar>
2014-09-03 11:52:58 -0300

    Modificados generadores de tests case.
    No podían correr en los labos porque la versión de ruby es vieja.

--- tp1/tests/ej1.random.rb
+++ tp1/tests/ej1.random.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
-r = Random.new((ARGV[0] && ARGV[0].to_i) || 0)
+srand((ARGV[0] && ARGV[0].to_i) || 0)
 n = (ARGV[1] && ARGV[1].to_i) || 100
-c = (ARGV[2] && ARGV[2].to_i) || (1 + r.rand((1.2*n).to_i))
+c = (ARGV[2] && ARGV[2].to_i) || (1 + rand((1.2*n).to_i))
 
 $stdout << "#{n} #{c}"
 1.upto(n) do 
-  $stdout << " #{r.rand(2)}"
+  $stdout << " #{rand(2)}"
 end
 $stdout << "\n"
 
--- tp1/tests/ej3.random.rb
+++ tp1/tests/ej3.random.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
-r = Random.new((ARGV[0] && ARGV[0].to_i) || 0)
+r = srand((ARGV[0] && ARGV[0].to_i) || 0)
 n = (ARGV[1] && ARGV[1].to_i) || 100
-m = (ARGV[2] && ARGV[2].to_i) || r.rand(1..n)
+m = (ARGV[2] && ARGV[2].to_i) || (1 + rand(n))
 
 puts "#{n} #{m}"
 2.upto(n) do |i|
-  $stdout << "#{r.rand n}"
+  $stdout << "#{rand n}"
   (i + 1).upto(n) do
-    $stdout << " #{r.rand n}"
+    $stdout << " #{rand n}"
   end
   puts
 end

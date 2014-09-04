c8590868b6f28a1aef38b2a52887de52b08ef861
Lucas Gabriel Vuotto <lvuotto@dc.uba.ar>
2014-09-03 23:52:31 -0300

    Agregado generador de peor caso de ejercicio 2.

--- tp1/tests/ej2.peor.rb
+++ tp1/tests/ej2.peor.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
-n = ARGV[0].to_i
+r = srand((ARGV[0] && ARGV[0].to_i) || 0)
+n = (ARGV[1] && ARGV[1].to_i) || 100
 
 puts "#{n}"
-
-1.upto(n) do |i|
-  puts "1 #{i} 3"
+1.upto(n) do |h|
+  pd = 20
+  pi = 1
+  puts "#{pi} #{h} #{pd}"
 end
-
 puts "0"

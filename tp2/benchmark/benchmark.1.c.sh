a9a1c04b8c4a413827026b7e62019bf81b11737f
lvuotto <lvuotto@dc.uba.ar>
2014-11-03 14:54:14 -0300

    Reducción cantidad de corridas.

--- tp2/benchmark/benchmark.1.c.sh
+++ tp2/benchmark/benchmark.1.c.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in {1..1000}; do
+for i in {1..200}; do
   $TESTDIR/ej1.random.rb 1 $((10*i)) 10000 1000 | $BINDIR/ej1 > /dev/null
 done
--- tp2/benchmark/benchmark.3.dnc.sh
+++ tp2/benchmark/benchmark.3.dnc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in {1..1000}; do
+for i in {1..200}; do
   $TESTDIR/ej3.random_denso_desconexo.rb 42 $((100*i)) 1000 | $BINDIR/ej3 > /dev/null
 done

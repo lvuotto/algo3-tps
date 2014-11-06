e0b92e5f16264fde676c6baf16c1dbc803af97ba
lvuotto <lvuotto@dc.uba.ar>
2014-11-05 23:20:26 -0300

    Corrección de typo.

--- tp2/benchmark/benchmark.3.m.sh
+++ tp2/benchmark/benchmark.3.m.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in {1..1000}; do
-  $TESTDIR/ej3.random.rb 42 1000 $((1000 + 999*i)) 1000 | $BINDIR/ej3 > /dev/null
+  $TESTDIR/ej3.random_resoluble.rb 42 1000 $((999*i)) 1000 | $BINDIR/ej3 > /dev/null
 done

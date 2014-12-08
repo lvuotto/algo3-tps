d8f218be52bab6a5059a0083a486d9e222b70af4
Lucas Gabriel Vuotto <lvuotto@dc.uba.ar>
2014-12-08 00:41:02 -0300

    Fix parámetros faltantes.

--- tp3/benchmark/generadores/estrella.rb
+++ tp3/benchmark/generadores/estrella.rb
@@ -5,6 +5,7 @@ n = ARGV[1] ? ARGV[1].to_i : 1 + rand(100)
 k = ARGV[2] ? ARGV[2].to_i : 1 + rand(100)
 
 peso_maximo = 15_000
+puts "#{n} #{n - 1} #{k}"
 2.upto(n) do |v|
   begin
     puts "1 #{v} #{rand(peso_maximo) / 100.0}"

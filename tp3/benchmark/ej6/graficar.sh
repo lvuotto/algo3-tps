15bda5bd3016bbb207881ee24fd13bf5beea34bb
Pablo González Alba <pablo.gonzalez.alba@gmail.com>
2014-12-15 18:23:25 -0300

    Separación del exacto en lso graficos de tiempo del ej6

--- tp3/benchmark/ej6/graficar.sh
+++ tp3/benchmark/ej6/graficar.sh
@@ -7,6 +7,16 @@ gnuplot -e "datafile='benchmark/ej6/completo_k7.dat'" -e "outfile='informe/image
 gnuplot -e "datafile='benchmark/ej6/arbol_k3.dat'" -e "outfile='informe/imagenes/ej6-arbol-k3-tiempo.png'" benchmark/ej6/plotter.tiempo.gnu 2>&1
 gnuplot -e "datafile='benchmark/ej6/arbol_k7.dat'" -e "outfile='informe/imagenes/ej6-arbol-k7-tiempo.png'" benchmark/ej6/plotter.tiempo.gnu 2>&1
 
+gnuplot -e "datafile='benchmark/ej6/denso_pesos_distintos_k3.dat'" -e "outfile='informe/imagenes/ej6-denso-pesos-distintos-k3-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+gnuplot -e "datafile='benchmark/ej6/denso_pesos_distintos_k7.dat'" -e "outfile='informe/imagenes/ej6-denso-pesos-distintos-k7-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+gnuplot -e "datafile='benchmark/ej6/denso_pesos_iguales_k3.dat'" -e "outfile='informe/imagenes/ej6-denso-pesos-iguales-k3-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+gnuplot -e "datafile='benchmark/ej6/denso_pesos_iguales_k7.dat'" -e "outfile='informe/imagenes/ej6-denso-pesos-iguales-k7-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+gnuplot -e "datafile='benchmark/ej6/completo_k3.dat'" -e "outfile='informe/imagenes/ej6-completo-k3-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+gnuplot -e "datafile='benchmark/ej6/completo_k7.dat'" -e "outfile='informe/imagenes/ej6-completo-k7-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+gnuplot -e "datafile='benchmark/ej6/arbol_k3.dat'" -e "outfile='informe/imagenes/ej6-arbol-k3-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+gnuplot -e "datafile='benchmark/ej6/arbol_k7.dat'" -e "outfile='informe/imagenes/ej6-arbol-k7-tiempo-exacto.png'" benchmark/ej6/plotter.tiempo.exacto.gnu 2>&1
+
+
 gnuplot -e "datafile='benchmark/ej6/denso_pesos_distintos_k3.dat'" -e "outfile='informe/imagenes/ej6-denso-pesos-distintos-k3-peso.png'" benchmark/ej6/plotter.peso.gnu 2>&1
 gnuplot -e "datafile='benchmark/ej6/denso_pesos_distintos_k7.dat'" -e "outfile='informe/imagenes/ej6-denso-pesos-distintos-k7-peso.png'" benchmark/ej6/plotter.peso.gnu 2>&1
 gnuplot -e "datafile='benchmark/ej6/denso_pesos_iguales_k3.dat'" -e "outfile='informe/imagenes/ej6-denso-pesos-iguales-k3-peso.png'" benchmark/ej6/plotter.peso.gnu 2>&1
--- tp3/benchmark/ej6/plotter.peso.gnu
+++ tp3/benchmark/ej6/plotter.peso.gnu
@@ -7,4 +7,4 @@ plot datafile u 1:3 smooth bezier with lines title 'GRASP repetir mejor 34 veces
   datafile u 1:5 smooth bezier with lines title 'Exacto',\
   datafile u 1:7 smooth bezier with lines title 'Goloso sin aleatorizar',\
   datafile u 1:9 smooth bezier with lines title 'GRASP aleatorizar ambos',\
-  datafile u 1:11 smooth bezier with lines title 'GRASP intercambiar'
+  datafile u 1:11 smooth bezier with lines title 'GRASP intercambiar' lt rgb '#000000'
